e9118cc0e5778feb04f869539848ba98df2d2820
Sukhmanbir Kaur <sukhmanbir.kaur@outlook.com>
2016-06-16 22:56:40 -0400

    Initial Commit

---
+++ KellyClarkson/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace KellyClarkson
+{
+    partial class kellyClarksonForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.outputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // outputRichTextBox
+            // 
+            this.outputRichTextBox.Location = new System.Drawing.Point(12, 50);
+            this.outputRichTextBox.Name = "outputRichTextBox";
+            this.outputRichTextBox.Size = new System.Drawing.Size(515, 349);
+            this.outputRichTextBox.TabIndex = 0;
+            this.outputRichTextBox.Text = "";
+            this.outputRichTextBox.TextChanged += new System.EventHandler(this.outputRichTextBox_TextChanged);
+            // 
+            // kellyClarksonForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 411);
+            this.Controls.Add(this.outputRichTextBox);
+            this.Name = "kellyClarksonForm";
+            this.Text = "Kelly Clarkson";
+            this.Load += new System.EventHandler(this.kellyClarksonForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox outputRichTextBox;
+    }
+}
+
---
+++ KellyClarkson/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KellyClarkson
+{
+    public partial class kellyClarksonForm : Form
+    {
+        const int five = 5;
+        const int three = 3;
+        int i;
+
+        public kellyClarksonForm()
+        {
+            InitializeComponent();
+        }
+
+        private void outputRichTextBox_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void kellyClarksonForm_Load(object sender, EventArgs e)
+        {
+            for (i = 1; i <= 100; i++)
+            {
+                if (i % three == 0 && i % five == 0)
+                {
+                    outputRichTextBox.Text += "Kelly Clarkson\n\n";
+                }
+                else if (i % five == 0)
+                {
+                    outputRichTextBox.Text += "Clarkson\n\n";
+                }
+                else if (i % three == 0)
+                {
+                    outputRichTextBox.Text += "Kelly\n\n";
+                }
+                else if (i % three != 0 && i % five != 0)
+                {
+                    int number = i;
+                    outputRichTextBox.Text += number.ToString() + "\n\n";
+                }
+            }
+        }
+    }
+}
